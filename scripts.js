9e8c5ff5b70b7231da9b6c26cdf4fbbc64a9659e
Jordan <uberbomb@yahoo.com>
2022-11-01 22:47:22 -0400

    Edit DOM manipulation and data processing functions

--- scripts.js
+++ scripts.js
@@ -1,39 +1,39 @@
 async function getWeather(city, unit) {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=03bba837610ec0c4d9d49862f2c39baf&units=imperial`);
-  const data = await response.json();
-  const weather = {
+  try {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=03bba837610ec0c4d9d49862f2c39baf&units=${unit}`);
+    const data = await response.json();
+    console.log(data);
+    const weather = {
     location: data.name,
     description: data.weather[0].description,
     temperature: data.main.temp,
-    feels_like: data.main.feels_like,
+    feelsLike: data.main.feels_like,
     humidity: data.main.humidity,
-    wind_speed: data.wind.speed
-  };
-  return weather;
-}
-
-function populateDOM(key, value) {
-  const div = document.getElementById(key).childNodes;
-  let header = formatHeader(key)
-  let data = formatData(value, header);
-  div[1].innerText = header;
-  div[3].innerText = data;
-}
-
-function formatHeader(key) {
-  if(key.includes("_")) {
-    const arr = key.split("_");
-    const newArr = [];
-    for(let item of arr) {
-      newArr.push(item[0].toUpperCase() + item.slice(1).toLowerCase());
-    }
-    let header = newArr.join(' ');
-    return header;
+    windSpeed: data.wind.speed
+    };
+    return weather;
+  } catch(err) {
+    console.log('Error. City not found.');
   }
-  return key[0].toUpperCase() + key.slice(1).toLowerCase();
 }
 
-function formatData(value, header) {
+function populateDOM(weatherData) {
+  const title = document.getElementById('title').childNodes;
+  const feelsLike = document.getElementById('feels-like').childNodes
+  const humidity = document.getElementById('humidity').childNodes
+  const windSpeed = document.getElementById('wind-speed').childNodes
+  title[1].innerText = weatherData.location;
+  title[3].innerText = weatherData.description;
+  title[5].innerText = weatherData.temperature;
+  feelsLike[1].innerText = 'Feels Like';
+  feelsLike[3].innerText = weatherData.feelsLike;
+  humidity[1].innerText = 'Humidity';
+  humidity[3].innerText = weatherData.humidity;
+  windSpeed[1].innerText = 'Wind Speed';
+  windSpeed[3].innerText = weatherData.windSpeed;
+}
+
+function formatData(value, header, unit) {
   if(isNaN(value) && value.includes(" ")) {
     const arr = value.split(" ");
     const newArr = [];
@@ -43,15 +43,29 @@ function formatData(value, header) {
     let data = newArr.join(' ');
     return data;
   }
-  switch(header) {
-    case 'Temperature':
-      return value + ' \u00B0F'
-    case 'Feels Like':
-      return value + ' \u00B0F'
-    case 'Humidity':
-      return value + '%'
-    case 'Wind Speed':
-      return value + ' MPH'
+  
+  if(unit === "imperial"){
+    switch(header) {
+      case 'temperature':
+        return value + ' \u00B0F'
+      case 'feelsLike':
+        return value + ' \u00B0F'
+      case 'humidity':
+        return value + '%'
+      case 'windSpeed':
+        return value + ' MPH'
+    }
+  } else {
+    switch(header) {
+      case 'temperature':
+        return value + ' \u00B0C'
+      case 'feelsLike':
+        return value + ' \u00B0C'
+      case 'humidity':
+        return value + '%'
+      case 'windSpeed':
+        return value + ' MPS'
+    }
   }
   return value
 }
@@ -59,9 +73,13 @@ function formatData(value, header) {
 const btn = document.querySelector('button');
 const input = document.querySelector('input[type="text"]');
 btn.addEventListener('click', () => {
-  getWeather(input.value).then(weatherInfo => {
+  let unit = document.querySelector('input[type=radio]:checked').value;
+  getWeather(input.value, unit).then(weatherInfo => {
+    let formattedWeatherInfo = {};
     for(let [key, value] of Object.entries(weatherInfo)) {
-      populateDOM(key, value);
+      let data = formatData(value, key, unit);
+      formattedWeatherInfo[key] = data;
     }
+    populateDOM(formattedWeatherInfo);
   });
 });
