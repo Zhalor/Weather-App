0c4116fa7090f63807f558324182a7acab7bf242
Jordan <uberbomb@yahoo.com>
2022-11-02 18:25:00 -0400

    Adjust styling and refactor functions
    Adjusted the layout and refactored some functions to fit and provide
    data for the new layout.

--- scripts.js
+++ scripts.js
@@ -1,39 +1,49 @@
 async function getWeather(city, unit) {
-  try {
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=03bba837610ec0c4d9d49862f2c39baf&units=${unit}`);
     const data = await response.json();
-    console.log(data);
     const weather = {
-    location: data.name,
-    description: data.weather[0].description,
-    temperature: data.main.temp,
-    feelsLike: data.main.feels_like,
-    humidity: data.main.humidity,
-    windSpeed: data.wind.speed
+      location: data.name,
+      country: data.sys.country,
+      description: data.weather[0].description,
+      temperature: data.main.temp,
+      high: data.main.temp_max,
+      low: data.main.temp_min,
+      feelsLike: data.main.feels_like,
+      humidity: data.main.humidity,
+      windSpeed: data.wind.speed,
+      icon: data.weather[0].icon,
+      timezone: data.timezone
     };
     return weather;
-  } catch(err) {
-    console.log('Error. City not found.');
-  }
 }
 
 function populateDOM(weatherData) {
-  const title = document.getElementById('title').childNodes;
-  const feelsLike = document.getElementById('feels-like').childNodes
-  const humidity = document.getElementById('humidity').childNodes
-  const windSpeed = document.getElementById('wind-speed').childNodes
-  title[1].innerText = weatherData.location;
-  title[3].innerText = weatherData.description;
-  title[5].innerText = weatherData.temperature;
-  feelsLike[1].innerText = 'Feels Like';
-  feelsLike[3].innerText = weatherData.feelsLike;
-  humidity[1].innerText = 'Humidity';
-  humidity[3].innerText = weatherData.humidity;
-  windSpeed[1].innerText = 'Wind Speed';
-  windSpeed[3].innerText = weatherData.windSpeed;
+  const weatherDataContainer = document.getElementById('weather-data-container');
+  const locationPara = document.getElementById('location');
+  const timePara = document.getElementById('time');
+  const tempH2 = document.getElementById('temp');
+  const tempHighSpan = document.getElementById('temp-high');
+  const tempLowSpan = document.getElementById('temp-low');
+  const weatherIcon = document.getElementById('weather-icon');
+  const descPara = document.getElementById('description');
+  const feelsLikeSpan = document.getElementById('feels-like');
+  const humidityPara = document.getElementById('humidity');
+  const windSpeedPara = document.getElementById('wind-speed');
+  
+  weatherDataContainer.style.display = 'flex';
+  locationPara.innerText = `${weatherData.location}, ${weatherData.country}`;
+  timePara.innerText = weatherData.timezone;
+  tempH2.innerText = weatherData.temperature;
+  tempHighSpan.innerText = weatherData.high;
+  tempLowSpan.innerText = weatherData.low;
+  weatherIcon.src = `http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
+  descPara.innerText = weatherData.description;
+  feelsLikeSpan.innerText = weatherData.feelsLike;
+  humidityPara.innerText = weatherData.humidity;
+  windSpeedPara.innerText = weatherData.windSpeed;
 }
 
-function formatData(value, header, unit) {
+function formatData(value, key, unit) {
   if(isNaN(value) && value.includes(" ")) {
     const arr = value.split(" ");
     const newArr = [];
@@ -42,29 +52,65 @@ function formatData(value, header, unit) {
     }
     let data = newArr.join(' ');
     return data;
+  } else if(!isNaN(value)) {
+    value = Math.round(value * 10) / 10;
   }
   
   if(unit === "imperial"){
-    switch(header) {
+    switch(key) {
       case 'temperature':
-        return value + ' \u00B0F'
+        return value + '\u00B0F'
+      case 'high':
+        return value + '\u00B0F'
+      case 'low':
+        return value + '\u00B0F'
       case 'feelsLike':
-        return value + ' \u00B0F'
+        return value + '\u00B0F'
       case 'humidity':
         return value + '%'
       case 'windSpeed':
-        return value + ' MPH'
+        return value + ' MpH'
+      case 'timezone':
+        const date = new Date();
+        let utcHours = date.getUTCHours();
+        let minutes = date.getUTCMinutes();
+        let locationHours = (value / 60) / 60;
+        let hours = locationHours + utcHours;
+        if(hours > 24) {
+          hours = hours - 24;
+        }
+        if(minutes < 10) {
+          minutes = `0${minutes}`;
+        }
+        return `${hours}:${minutes}`;
     }
   } else {
-    switch(header) {
+    switch(key) {
       case 'temperature':
-        return value + ' \u00B0C'
+        return value + '\u00B0C'
+      case 'high':
+        return value + '\u00B0C'
+      case 'low':
+        return value + '\u00B0C'
       case 'feelsLike':
-        return value + ' \u00B0C'
+        return value + '\u00B0C'
       case 'humidity':
         return value + '%'
       case 'windSpeed':
-        return value + ' MPS'
+        return value + ' MpS'
+      case 'timezone':
+        const date = new Date();
+        let utcHours = date.getUTCHours();
+        let minutes = date.getUTCMinutes();
+        let locationHours = (value / 60) / 60;
+        let hours = locationHours + utcHours;
+        if(hours > 24) {
+          hours = hours - 24;
+        }
+        if(minutes < 10) {
+          minutes = `0${minutes}`;
+        }
+        return `${hours}:${minutes}`;
     }
   }
   return value
@@ -74,12 +120,18 @@ const btn = document.querySelector('button');
 const input = document.querySelector('input[type="text"]');
 btn.addEventListener('click', () => {
   let unit = document.querySelector('input[type=radio]:checked').value;
-  getWeather(input.value, unit).then(weatherInfo => {
+  getWeather(input.value.trim(), unit).then(weatherInfo => {
     let formattedWeatherInfo = {};
     for(let [key, value] of Object.entries(weatherInfo)) {
       let data = formatData(value, key, unit);
       formattedWeatherInfo[key] = data;
     }
     populateDOM(formattedWeatherInfo);
+    input.value = '';
+  }).catch(err => {
+    document.getElementById('weather-data-container').style.display = 'none';
+    document.getElementById('location').innerText = 'City not found';
+    document.getElementById('time').innerText = '';
   });
 });
+
